cb3bbe89ab8979fbba482803e16c78b63e71c384
dimonDDL <dimin21590@gmail.com>
2023-03-20 00:01:49 +0100

    cleaned up and sliglty optimized streamer

--- Streamer/src/main.rs
+++ Streamer/src/main.rs
@@ -15,7 +15,7 @@ fn pritntBuffer(buffer: &[u8]){
         }
         // print the binary representation of the value
         // print!("{:08b}", buffer[i]);
-        if(buffer[i] > 50){
+        if buffer[i] > 50 {
             print!("1");
         }else{
             print!("0");
@@ -28,10 +28,10 @@ fn main() {
     let target_x_size = 320;
     let target_y_size = 240;
 
-    let x_offset = 0;
-    let y_offset = 0;
+    let x_offset = 128;
+    let y_offset = 128;
 
-    let threshold = 80;
+    let threshold = 100;
     let smoothing = false;
 
     let magic:char = 'A';
@@ -40,8 +40,8 @@ fn main() {
     let x_step = 1;
     let y_step = x_step;
 
-    let port = SerialPort::open("/dev/ttyACM1", 115200);
-    let mut ser_port;
+    let port = SerialPort::open("/dev/ttyACM0", 115200);
+    let ser_port;
     match port {
         Ok(port) => {
             println!("Port opened");
@@ -55,7 +55,7 @@ fn main() {
     }
 
     let one_second = Duration::new(1, 0);
-    let one_frame = one_second / 60;
+    let one_frame = one_second / 120;
 
     let display = Display::primary().expect("Couldn't find primary display.");
     let mut capturer = Capturer::new(display).expect("Couldn't begin capture.");
@@ -63,14 +63,12 @@ fn main() {
 
     println!("x_step: {}, y_step: {}", x_step, y_step);
     println!("Window size: {}, {}", x_step*target_x_size, y_step*target_y_size);
-    //thread::sleep(Duration::from_millis(2000));
-
-    // a buffer of grayscale values
-    let mut target_buffer:Vec<u8> = vec![0; target_x_size * target_y_size];    
+    //thread::sleep(Duration::from_millis(2000));  
 
     let mut last_loop = std::time::Instant::now();
+    let mut n_loops = 0;
     loop {
-
+        n_loops += 1;
         // Wait until there's a frame.
         let buffer = match capturer.frame() {
             Ok(buffer) => buffer,
@@ -84,28 +82,29 @@ fn main() {
             }
         };
 
-        println!("Captured! tdelta: {}us", std::time::Instant::now().duration_since(last_loop).as_micros());
-        last_loop = std::time::Instant::now();
+        // create the u8 buffer to be sent
+        let mut data_buffer:Vec<u8> = vec![0; target_x_size * target_y_size / 4];
+        // a buffer of grayscale values
+        let mut target_buffer:Vec<u8> = vec![0; target_x_size * target_y_size];
 
         let stride = buffer.len() / h;
-
         let mut iter = 0;
         for y in 0..target_y_size {
             for x in 0..target_x_size {
                 // convert coordinates from the target coordinates to the screen coordinates
-                let bigX = x * x_step + x_offset;
-                let bigY = y * y_step + y_offset;
+                let big_x = x * x_step + x_offset;
+                let big_y = y * y_step + y_offset;
                 // check if the coordinates are valid and aren't outside the screen
-                if bigX >= w || bigY >= h {
+                if big_x >= w || big_y >= h {
                     continue;
                 }
 
-                let mut pixel:u8 = 0;
+                let pixel:u8;
                 if smoothing {
                     let mut accumulator = 0;
                     for i in 0..x_step {
                         for j in 0..y_step {
-                            let i = stride * (bigY + j) + 4 * (bigX + i);
+                            let i = stride * (big_y + j) + 4 * (big_x + i);
                             let r = buffer[i + 2];
                             let g = buffer[i + 1];
                             let b = buffer[i];
@@ -115,25 +114,18 @@ fn main() {
                     let avg = accumulator / (x_step * y_step) as u16;
                     pixel = avg as u8;
                 }else{
-                    let i = stride * bigY + 4 * bigX;
+                    let i = stride * big_y + 4 * big_x;
                     let r = buffer[i + 2];
                     let g = buffer[i + 1];
                     let b = buffer[i];
                     let avg = (r as u16 + g as u16 + b as u16)/3;
                     pixel = avg as u8;
-                    //println!("Reading: X: {}, Y: {}, STRIDE: {}; R: {}, G: {}, B: {} - {} ", bigX, bigY, stride, r, g, b, pixel);
                 }
-                
                 target_buffer[iter] = pixel;
                 iter += 1;
             }
         }
-        
-        // pritntBuffer(&target_buffer);
 
-        // now we create the u8 buffer to be sent
-        let mut data_buffer:Vec<u8> = vec![0; target_buffer.len() / 4];
-        // iterate in 16 bit chunks
         for i in 0..target_buffer.len()/4 {
             // threshold represents the maximum brightness value
             // so if a pixel is > threshold, we set it to 11
@@ -167,8 +159,6 @@ fn main() {
             }
 
             data_buffer[i] = byte;
-            // print the byte
-            // println!("{}: {:08b}", i, byte);
         }
 
         loop {
@@ -176,17 +166,24 @@ fn main() {
             let mut buffer = [0; 1];
             let read = ser_port.read(&mut buffer);
             match read {
-                Ok(read) => {
+                Ok(_read) => {
                     // if we read a magic char, we can send the data
                     if buffer[0] as char == magic {
                         ser_port.write(&data_buffer);
                         break;
                     }
                 }
-                Err(e) => {
+                Err(_e) => {
                     continue;
                 }
             }
+        }
+
+        if(last_loop).elapsed().as_secs() >= 1 {
+            println!("FPS: {}", n_loops);
+            // println!("Captured! tdelta: {}us", std::time::Instant::now().duration_since(last_loop).as_micros());
+            last_loop = std::time::Instant::now();
+            n_loops = 0;
         }
 
         //break;
