4fe289938f42cd045b248172bcdeb27918392f62
dimonDDL <dimin21590@gmail.com>
2023-05-12 15:00:08 +0200

    cleaned up the code and added some optimizations

--- rp2040/main.cpp
+++ rp2040/main.cpp
@@ -41,27 +41,14 @@ volatile uint8_t iteration = 0;
 
 
 // internal use variables
-int dma_chan;
+int dma_chan1;
+int dma_chan2;
+dma_channel_config dma_conf1;
+dma_channel_config dma_conf2;
 PIO pio;
 uint smData;
 uint smClock;
 const uint ledPin = 25;
-
-// function that allows us to use the BOOTSEL button as user input
-bool __no_inline_not_in_flash_func(get_bootsel_button)() {
-    const uint CS_PIN_INDEX = 1;
-    uint32_t flags = save_and_disable_interrupts();
-    hw_write_masked(&ioqspi_hw->io[CS_PIN_INDEX].ctrl,
-                    GPIO_OVERRIDE_LOW << IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_LSB,
-                    IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_BITS);
-    for (volatile int i = 0; i < 1000; ++i);
-    bool button_state = !(sio_hw->gpio_hi_in & (1u << CS_PIN_INDEX));
-    hw_write_masked(&ioqspi_hw->io[CS_PIN_INDEX].ctrl,
-                    GPIO_OVERRIDE_NORMAL << IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_LSB,
-                    IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_BITS);
-    restore_interrupts(flags);
-    return button_state;
-}
 
 void __time_critical_func(compute_framebuffer)(bool currBuff) {
     // check each pixel value in frameBuffer and the value of iteration, if the pixel is less than iteration, set it to 0 else set it to 1
@@ -94,65 +81,6 @@ void fill_buffer(uint type = 0){
                 frameBuffer[y][x] = 1;
             }
         }
-    }else if(type == 2){
-       for(int y = 0; y < displayYsize; y++){
-            for(int x = 0; x < displayXsize; x++){
-                // if x is even then set to 0xFF else 0x00
-                frameBuffer[y][x] = (x % 2 == 0) ? 0xFF : 0x00;
-            }
-        } 
-    }else if(type == 3){
-        // fill in checkerboard pattern
-        for(int y = 0; y < displayYsize; y++){
-            for(int x = 0; x < displayXsize; x++){
-                // if x is even then set to 0xFF else 0x00
-                if(y % 2 == 0)
-                    frameBuffer[y][x] = (x % 2 == 0) ? 0xFF : 0x00;
-                else
-                    frameBuffer[y][x] = (x % 2 == 0) ? 0x00 : 1;
-            }
-        }
-    }else if(type == 4){
-        // fill the display with iterating brightness values
-        uint brightness = 0;
-        for(int y = 0; y < displayYsize; y++){
-            for(int x = 0; x < displayXsize; x++){
-                frameBuffer[y][x] = brightness;
-                brightness++;
-                if(brightness > brRollover)
-                    brightness = 0;
-            }
-        }
-    }else if(type == 5){
-        fill_buffer(0);
-        // fill a few very specific pixels
-        frameBuffer[0][0] = 0xFF;
-        frameBuffer[0][1] = 0xFF;
-        frameBuffer[0][2] = 0xFF;
-        frameBuffer[0][3] = 0xFF;
-        frameBuffer[displayYsize - 1][0] = 0xFF;
-        frameBuffer[displayYsize - 1][1] = 0xFF;
-        frameBuffer[displayYsize - 1][2] = 0xFF;
-        frameBuffer[displayYsize - 1][3] = 0xFF;
-        frameBuffer[0][displayXsize - 1] = 0xFF;
-        frameBuffer[0][displayXsize - 2] = 0xFF;
-        frameBuffer[0][displayXsize - 3] = 0xFF;
-        frameBuffer[0][displayXsize - 4] = 0xFF;
-        frameBuffer[displayYsize - 1][displayXsize - 1] = 0xFF;
-        frameBuffer[displayYsize - 1][displayXsize - 2] = 0xFF;
-        frameBuffer[displayYsize - 1][displayXsize - 3] = 0xFF;
-        frameBuffer[displayYsize - 1][displayXsize - 4] = 0xFF;
-    }else if(type == 6){
-        // fill the display with random values
-        for(int y = 0; y < displayYsize; y++){
-            for(int x = 0; x < displayXsize; x++){
-                frameBuffer[y][x] = ((x+offset)^(y+offset)) % ~9;
-            }
-        }
-        offset++;
-    }else if(type == 7){
-        fill_buffer(0);
-        // nothing for now
     }else{
         for(int y = 0; y < displayYsize; y++){
             for(int x = 0; x < displayXsize; x++){
@@ -166,7 +94,7 @@ void __time_critical_func(configureDMA)(bool currBuff);
 
 void __time_critical_func(dma_irq_handler)(){
     // acknowledge the interrupt
-    dma_hw->ints0 = (1u << dma_chan);
+    dma_hw->ints0 = (1u << dma_chan1) | (1u << dma_chan2);
     if(computeComplete){
         iteration++;
         currentBuffer = !currentBuffer;
@@ -179,37 +107,27 @@ void __time_critical_func(dma_irq_handler)(){
 }
 
 void __time_critical_func(configureDMA)(bool currBuff){
-    // make a dma config
-    dma_channel_config dma_conf = dma_channel_get_default_config(dma_chan);
-    // transfer uint32_t
-    channel_config_set_transfer_data_size(&dma_conf, DMA_SIZE_32);
-    // an array is read -> increment of read pointer
-    channel_config_set_read_increment(&dma_conf, true);
-    // a FIFO is written to -> no increment of write pointer
-    channel_config_set_write_increment(&dma_conf, false);
-    // let writing to the SM determine the speed of DMA
-    channel_config_set_dreq(&dma_conf, pio_get_dreq(pio, smData, true));
-    // set the source and destination address
+    irq_set_exclusive_handler(DMA_IRQ_0, dma_irq_handler);
+    irq_set_enabled(DMA_IRQ_0, true);
     if(currBuff){
-        dma_channel_configure(dma_chan, &dma_conf,
+        dma_channel_configure(dma_chan1, &dma_conf1,
                               &pio->txf[smData],                    // Destinatinon pointer
                               &intermediaryFrameBuffer1,            // Source pointer
                               (displayYsize * displayXsizeDiv32),   // Number of transfers
                               false                                 // Start immediately
         );
+        dma_channel_set_irq0_enabled(dma_chan1, true);
+        dma_channel_start(dma_chan1);
     }else{
-        dma_channel_configure(dma_chan, &dma_conf,
+        dma_channel_configure(dma_chan2, &dma_conf2,
                               &pio->txf[smData],                    // Destinatinon pointer
                               &intermediaryFrameBuffer2,            // Source pointer
                               (displayYsize * displayXsizeDiv32),   // Number of transfers
                               false                                 // Start immediately
         );
+        dma_channel_set_irq0_enabled(dma_chan2, true);
+        dma_channel_start(dma_chan2);
     }
-    // configure DMA interrupts
-    dma_channel_set_irq0_enabled(dma_chan, true);
-    irq_set_exclusive_handler(DMA_IRQ_0, dma_irq_handler);
-    irq_set_enabled(DMA_IRQ_0, true);
-    dma_channel_start(dma_chan);
 }
 
 void __time_critical_func(core1)(){
@@ -257,24 +175,42 @@ int main()
     fill_buffer(1);
 
     // Get a free channel, panic() if there are none
-    dma_chan = dma_claim_unused_channel(true);
+    dma_chan1 = dma_claim_unused_channel(true);
+    dma_chan2 = dma_claim_unused_channel(true);
+
+    dma_conf1 = dma_channel_get_default_config(dma_chan1);
+    channel_config_set_transfer_data_size(&dma_conf1, DMA_SIZE_32);
+    channel_config_set_read_increment(&dma_conf1, true);
+    channel_config_set_write_increment(&dma_conf1, false);
+    channel_config_set_dreq(&dma_conf1, pio_get_dreq(pio, smData, true));
+
+    dma_conf2 = dma_channel_get_default_config(dma_chan2);
+    channel_config_set_transfer_data_size(&dma_conf2, DMA_SIZE_32);
+    channel_config_set_read_increment(&dma_conf2, true);
+    channel_config_set_write_increment(&dma_conf2, false);
+    channel_config_set_dreq(&dma_conf2, pio_get_dreq(pio, smData, true));
 
     compute_framebuffer(currentBuffer);
     compute_framebuffer(!currentBuffer);
 
     configureDMA(currentBuffer);
+
+    int dma_chan3 = dma_claim_unused_channel(true);
+    dma_channel_config dma_conf_memcpy = dma_channel_get_default_config(dma_chan3);
+    channel_config_set_transfer_data_size(&dma_conf_memcpy, DMA_SIZE_8);
+    channel_config_set_read_increment(&dma_conf_memcpy, true);
+    channel_config_set_write_increment(&dma_conf_memcpy, true);
+
     gpio_put(ledPin, 1);
     multicore_launch_core1(core1);
+
+    // wait for USB to connect
     while (!tud_cdc_connected()) {
         tight_loop_contents();
     }
+
     while(true)
     {
-        // fill_buffer(6);
-        // sleep_ms(10);
-
-        // start time
-        //uint32_t startTime = time_us_32();
 
         puts("A");
         bool broke = false;
@@ -289,13 +225,13 @@ int main()
                         timeout = true;
                     }
                 }
-            
                 if(!timeout){
                     tud_cdc_read(buf, sizeof(buf));
                 }else{
                     broke = true;
                     break;
                 }
+                // we need to read two bytes at a time to avoid overhead
                 uint8_t c1 = (uint8_t)buf[0];
                 uint8_t c2 = (uint8_t)buf[1];
                 // byte structure:
@@ -326,12 +262,15 @@ int main()
             }
         }
         gpio_put(ledPin, broke);
-        // uint32_t deltaTime = time_us_32() - startTime;
-        // printf("Time: %d", deltaTime);
 
-        // copy tmpframeBuffer into the real frame buffer
+        // copy tmpframeBuffer into the real frame buffer (this is needed to avoid tearing)
         if(!broke){
-            memcpy(frameBuffer, tmpframeBuffer, sizeof(frameBuffer));
+            dma_channel_configure(dma_chan3, &dma_conf_memcpy,
+                frameBuffer,            // dst
+                tmpframeBuffer,         // src
+                sizeof(frameBuffer),    // transfer count
+                true                    // start immediately
+            );
         }
     }
     return 0;
